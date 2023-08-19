5726951ce8b1769a16bde30937c030034bfd7d0c
Ebuka <austinobravo@gmail.com>
2023-08-19 17:20:34 +0100

    Render whitenoise

--- selfmail/settings.py
+++ selfmail/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,7 @@ STATICFILES_DIRS = BASE_DIR / 'static/',
 MEDIA_URL ="/media/"
 MEDIA_ROOT = BASE_DIR / 'static/media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
